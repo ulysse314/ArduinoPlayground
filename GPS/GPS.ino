b49a73212f737291008cfd203e30d29c056ea5ec
moussaillon <moussaillon>
2019-12-17 02:36:36 +0100

    Adding gps test

---
+++ GPS/GPS.ino
@@ -0,0 +1,21 @@
+#include <Adafruit_GPS.h>
+
+Adafruit_GPS gps(&Serial1);
+
+void setup() {
+  Serial.begin(115200);
+  gps.begin(115200);
+  gps.sendCommand(PMTK_SET_NMEA_UPDATE_1HZ); // 1 Hz update rate
+  gps.sendCommand(PGCMD_ANTENNA);
+}
+
+void loop() {
+  if (Serial1.available()) {
+    gps.read();
+    if (gps.newNMEAreceived()) {
+      const char *nmea = gps.lastNMEA();
+      gps.parse(nmea);
+      Serial.print(nmea);
+    }
+  }
+}
